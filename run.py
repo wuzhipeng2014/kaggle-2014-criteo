50e45f44cd2f2820e0b20bd3ede3adf63a460912
zhipeng.wu <zhipeng.wu@qunar.com>
2017-08-12 16:23:27 +0800

    MOD:添加注释

--- run.py
+++ run.py
@@ -14,7 +14,7 @@ subprocess.call(cmd, shell=True)
 cmd = 'converters/parallelizer-a.py -s {nr_thread} converters/pre-a.py tr.csv tr.gbdt.dense tr.gbdt.sparse'.format(nr_thread=NR_THREAD)
 subprocess.call(cmd, shell=True) 
 
-#2.2 数字特征和分裂 特征分离及并行化处理 （测试数据）
+#2.2 数字特征和分类 特征分离及并行化处理 （测试数据）
 cmd = 'converters/parallelizer-a.py -s {nr_thread} converters/pre-a.py te.csv te.gbdt.dense te.gbdt.sparse'.format(nr_thread=NR_THREAD)
 subprocess.call(cmd, shell=True) 
 
@@ -27,11 +27,11 @@ subprocess.call(cmd, shell=True)
 # cmd = 'rm -f te.gbdt.dense te.gbdt.sparse tr.gbdt.dense tr.gbdt.sparse'
 # subprocess.call(cmd, shell=True)
 
-#5.1 训练数据并行预处理
+#5.1 训练数据并行预处理 (线性模型)
 cmd = 'converters/parallelizer-b.py -s {nr_thread} converters/pre-b.py tr.csv tr.gbdt.out tr.ffm'.format(nr_thread=NR_THREAD)
 subprocess.call(cmd, shell=True) 
 
-#5.2 测试数据并行预处理
+#5.2 测试数据并行预处理 （线性模型）
 cmd = 'converters/parallelizer-b.py -s {nr_thread} converters/pre-b.py te.csv te.gbdt.out te.ffm'.format(nr_thread=NR_THREAD)
 subprocess.call(cmd, shell=True) 
 
